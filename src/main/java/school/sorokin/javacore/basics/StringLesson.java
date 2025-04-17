53535ec53c06eeaec2ca12247565cbf3d03b7fe6
Shakh Shukhratov <shakhriplay@gmail.com>
2025-04-17 08:37:46 +0300

    homework

--- src/main/java/school/sorokin/javacore/basics/StringLesson.java
+++ src/main/java/school/sorokin/javacore/basics/StringLesson.java
@@ -2,6 +2,8 @@ package school.sorokin.javacore.basics;
 
 public class StringLesson {
     public static void main(String[] args) {
-
+        String sentence = "Java is cool and very popular.";
+        boolean containsCool = sentence.contains("cool");
+        System.out.println("Содержит 'cool'? " + containsCool);
     }
 }
