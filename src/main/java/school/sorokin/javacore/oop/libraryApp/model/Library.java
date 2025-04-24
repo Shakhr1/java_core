44ebb0811a0b8894e7fb120cbc86c83095f35abf
Shakh Shukhratov <shakhriplay@gmail.com>
2025-04-24 18:42:47 +0300

    add: class Library

---
+++ src/main/java/school/sorokin/javacore/oop/libraryApp/model/Library.java
@@ -0,0 +1,8 @@
+package school.sorokin.javacore.oop.libraryApp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+    private List<Book> catalog = new ArrayList<>();
+}
