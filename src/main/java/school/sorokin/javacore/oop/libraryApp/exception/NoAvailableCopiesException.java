22aa3f12ffa796f03f50aee86b556d7f9ba8fa29
Shakh Shukhratov <shakhriplay@gmail.com>
2025-04-24 22:26:03 +0300

    add: exception class

---
+++ src/main/java/school/sorokin/javacore/oop/libraryApp/exception/NoAvailableCopiesException.java
@@ -0,0 +1,7 @@
+package school.sorokin.javacore.oop.libraryApp.exception;
+
+public class NoAvailableCopiesException extends RuntimeException {
+    public NoAvailableCopiesException(String message) {
+        super(message);
+    }
+}
