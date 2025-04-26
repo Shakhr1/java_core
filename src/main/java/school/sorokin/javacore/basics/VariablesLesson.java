f983a0198f4ce46995b3ce7dd4096931c7269f4e
Shakh Shukhratov <shakhriplay@gmail.com>
2025-04-26 17:55:08 +0300

    Revert "fix"
    This reverts commit dfacf407d506d5255f222b69ee68496985e0b759.

--- src/main/java/school/sorokin/javacore/basics/VariablesLesson.java
+++ src/main/java/school/sorokin/javacore/basics/VariablesLesson.java
@@ -2,6 +2,7 @@ package school.sorokin.javacore.basics;
 
 public class VariablesLesson {
     public static void main(String[] args) {
+        // 1. Объявление и инициализация переменных
         byte flash = 69;
         short year = 2077;
         int population = 8_000_000;
@@ -11,6 +12,7 @@ public class VariablesLesson {
         boolean isTrue = false;
         char button = 'F';
 
+        // 2. Вывод значений с описанием
         System.out.println("flash: " + flash);
         System.out.println("year: " + year);
         System.out.println("population: " + population);
@@ -20,12 +22,15 @@ public class VariablesLesson {
         System.out.println("is true or not: " + isTrue);
         System.out.println("button: " + button);
 
+        // 3. Явное приведение long в int
         int onlineValve = (int) peekOnlineValve;
         System.out.println("onlineValve: " + onlineValve);
 
+        // Проверяем потерю данных
         long online = peekOnlineValve - onlineValve;
         System.out.println("Потеря данных: " + online);
 
+        // 4. Работа с char
         for (int i = 0; i < 3; i++) {
             System.out.println(button + i);
         }
