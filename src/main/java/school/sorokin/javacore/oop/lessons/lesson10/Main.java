e19286c9ea99236ed5f60588c16ef13fd6afbfe8
Shakh Shukhratov <shakhriplay@gmail.com>
2025-05-02 21:21:32 +0300

    test stream api

--- src/main/java/school/sorokin/javacore/oop/lessons/lesson10/Main.java
+++ src/main/java/school/sorokin/javacore/oop/lessons/lesson10/Main.java
@@ -2,12 +2,30 @@ package school.sorokin.javacore.oop.lessons.lesson10;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
-        Stream<String> streamFromList = names.stream();
+//        List<> names = Arrays.asList("Alice", "Bob", "Charlie");
+//        Stream<String> streamFromList = names.stream();
+//        System.out.println("Стрим из коллекции:");
+//        streamFromList.forEach(System.out::println);
 
+
+        List<Integer> numbers = new Random()
+                .ints(100,0,100)
+                .filter(i -> i % 2 == 0)
+                .limit(10)
+                .boxed()
+                .collect(Collectors.toList());
+        System.out.println(numbers);
+
+//        List<Integer> streamFromIntegers = numbers.stream()
+//                .filter(n -> n % 2 == 0)
+//                .limit(10)
+//                .collect(Collectors.toList());
+//        System.out.println(streamFromIntegers);
     }
 }
