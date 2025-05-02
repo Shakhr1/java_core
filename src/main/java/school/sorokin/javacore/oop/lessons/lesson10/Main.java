f1b5457d902a3bf2ebde9b8b7984f21f09e86740
Shakh Shukhratov <shakhriplay@gmail.com>
2025-05-02 21:57:50 +0300

    test stream api

--- src/main/java/school/sorokin/javacore/oop/lessons/lesson10/Main.java
+++ src/main/java/school/sorokin/javacore/oop/lessons/lesson10/Main.java
@@ -1,10 +1,7 @@
 package school.sorokin.javacore.oop.lessons.lesson10;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
