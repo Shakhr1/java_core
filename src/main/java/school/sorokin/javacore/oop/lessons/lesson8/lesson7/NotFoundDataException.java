902e534daed3ed7cf6c2c45a53663fa9edefbb02
Shakh Shukhratov <100706849+Shakhr1@users.noreply.github.com>
2025-05-06 22:52:39 +0300

    Online shop (#6)
    * add: customer order prod
    
    * upd some methods
    
    * fix and upd tasks
    
    * add pom.xml from master
    
    * add file idea
    
    * Add new method for create data

--- src/main/java/school/sorokin/javacore/oop/lessons/lesson8/lesson7/NotFoundDataException.java
+++
@@ -1,7 +0,0 @@
-package school.sorokin.javacore.oop.lessons.lesson8.lesson7;
-
-public class NotFoundDataException extends RuntimeException {
-    public NotFoundDataException(String message) {
-        super(message);
-    }
-}
